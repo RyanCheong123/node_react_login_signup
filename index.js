d4211ee6089001cf9106454490311c4129462bab
RyanCheong123 <rhhjjeong@hotmail.com>
2020-08-22 11:52:17 -0700

    security update

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key')
+const { User } = require("./models/User");
+
+//aplpication/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://ryancheong:hj1017@test-shard-00-00.iffex.mongodb.net:27017,test-shard-00-01.iffex.mongodb.net:27017,test-shard-00-02.iffex.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-ygv8le-shard-0&authSource=admin&retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/register', (req, res) => {
+  //To save signup info to DB
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({ success: true })
+  })
+
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
