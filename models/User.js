c5b9014fa3185ee2a755881434c759fdf97fd6cd
RyanCheong123 <rhhjjeong@hotmail.com>
2020-08-24 11:58:32 -0700

    signup decrypting password added

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRound = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -28,6 +30,28 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+    }
+})
+
+userSchema.pre('save', function ( next ) {
+    var user = this;
+
+    if(user.isModified('password')) {
+        //decrypting password
+        bcrypt.genSalt(saltRound, function (err, salt) {
+            if(err){
+                return 
+                next(err)
+            } 
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err){
+                    return 
+                    next(err)
+                }
+                user.password = hash;
+                next()
+            });
+        });
     }
 })
 
